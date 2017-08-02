3919520ab48f5fd6fb369b75c95986641fc58338
M <mail@mail.com>
2017-08-02 11:49:05 +0700

    ดรปำก

--- resources/views/page/donate/index.blade.php
+++ resources/views/page/donate/index.blade.php
@@ -51,10 +51,25 @@
 
         <div class="clearfix margin-bottom-10"></div>
 
-        <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+        <input type="hidden" name="cmd" value="_donations">
+        <input type="hidden" name="business" value="charityth.donation@gmail.com">
+        <input type="hidden" name="lc" value="BM">
+        <input type="hidden" name="item_name" value="Donation">
+        <input type="hidden" name="item_number" value="D001">
+        <input type="hidden" name="no_note" value="0">
+        <input type="hidden" name="currency_code" value="THB">
+        <input type="hidden" name="bn" value="PP-DonationsBF:btn_donateCC_LG.gif:NonHostedGuest">
+        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+        </form>
+
+        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
           <input type="hidden" name="cmd" value="_donations">
           <input type="hidden" name="business" value="charityth.donation@gmail.com">
+          <input type="hidden" name="lc" value="TH">
           <input type="hidden" name="item_name" value="Donate">
+          <input type="hidden" name="no_note" value="0">
           <input type="hidden" name="currency_code" value="THB">
           <button>
             บริจาคผ่าน
