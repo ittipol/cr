e6c292a8f5d735219d06be916000269a9eb8f556
M <mail@mail.com>
2017-08-26 10:36:03 +0700

    fixed

--- app/Models/Charity.php
+++ app/Models/Charity.php
@@ -5,7 +5,7 @@ namespace App\Models;
 class Charity extends Model
 {
   protected $table = 'charities';
-  protected $fillable = ['name','short_desc','description','charity_type_id','logo','thumbnail','vdo_url','shared_image','images','address','district_id','province_id','has_reward'];
+  protected $fillable = ['name','short_desc','description','charity_type_id','logo','thumbnail','vdo_url','shared_image','images','address','district_id','province_id','has_reward','active'];
 
   public function charityType() {
     return $this->hasOne('App\Models\CharityType','id','charity_type_id');
