ebf7c1cc99b066eef8423bcc16096fb8be41bfab
M <mail@mail.com>
2017-09-03 23:00:52 +0700

    fb px

--- resources/views/script/analyticstracking.blade.php
+++ resources/views/script/analyticstracking.blade.php
@@ -10,11 +10,11 @@
     t.src=v;s=b.getElementsByTagName(e)[0];
     s.parentNode.insertBefore(t,s)}(window, document,'script',
     'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '227375124451364');
+    fbq('init', '114564405900626');
     fbq('track', 'PageView');
   </script>
   <noscript><img height="1" width="1" style="display:none"
-    src="https://www.facebook.com/tr?id=227375124451364&ev=PageView&noscript=1"
+    src="https://www.facebook.com/tr?id=114564405900626&ev=PageView&noscript=1"
   /></noscript>
   <!-- End Facebook Pixel Code -->
 @endif
