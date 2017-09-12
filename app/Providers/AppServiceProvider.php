26791291b6e87004380b487c8cd6e939346ea9e5
M <mail@mail.com>
2017-09-12 20:47:39 +0700

    fixed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -35,36 +35,36 @@ class AppServiceProvider extends ServiceProvider
             ->count();
 
             view()->share('_id',request()->id);
-            view()->share('_totalNews',$news);
             view()->share('_totalProject',$projects);
+            view()->share('_totalNews',$news);
             view()->share('_totalVideo',$video);
 
         });
 
         view()->composer('page.charity.nav', function($view){
 
-            // $news = Service::loadModel('News')
-            // ->select('id','charity_id','title','short_desc','thumbnail','created_at')
-            // ->where('charity_id','=',request()->id)
-            // ->count();
+            $news = Service::loadModel('News')
+            ->select('id','charity_id','title','short_desc','thumbnail','created_at')
+            ->where('charity_id','=',request()->id)
+            ->count();
 
-            // $projects = Service::loadModel('Project')
-            // ->select('id','name','short_desc','thumbnail','end_date','target_amount')
-            // ->where([
-            //   ['charity_id','=',request()->id],
-            //   ['end_date','>',date('Y-m-d H:i:s')]
-            // ])
-            // ->count();
+            $projects = Service::loadModel('Project')
+            ->select('id','name','short_desc','thumbnail','end_date','target_amount')
+            ->where([
+              ['charity_id','=',request()->id],
+              ['end_date','>',date('Y-m-d H:i:s')]
+            ])
+            ->count();
 
-            // $video = Service::loadModel('Video')
-            // ->select('id','charity_id','title','short_desc','thumbnail','created_at')
-            // ->where('charity_id','=',request()->id)
-            // ->count();
+            $video = Service::loadModel('Video')
+            ->select('id','charity_id','title','short_desc','thumbnail','created_at')
+            ->where('charity_id','=',request()->id)
+            ->count();
 
             view()->share('_id',request()->id);
-            // view()->share('_totalNews',$news);
-            // view()->share('_totalProject',$projects);
-            // view()->share('_totalVideo',$video);
+            view()->share('_totalProject',$projects);
+            view()->share('_totalNews',$news);
+            view()->share('_totalVideo',$video);
 
         });
     }
--- resources/views/page/charity/nav.blade.php
+++ resources/views/page/charity/nav.blade.php
@@ -3,12 +3,15 @@
     <li @if($_pageName == 'Charity') class="active" @endif><a href="{{URL::to('charity')}}/{{$_id}}">หน้าหลัก</a></li>
     <li @if($_pageName == 'Project') class="active" @endif>
       <a href="{{URL::to('charity')}}/{{$_id}}/project">โครงการ</a>
+      <span class="badge badge-blue rounded-x">{{$_totalProject}}</span>
     </li>
     <li @if($_pageName == 'News') class="active" @endif>
       <a href="{{URL::to('charity')}}/{{$_id}}/news">ข่าวสาร</a>
+      <span class="badge badge-blue rounded-x">{{$_totalNews}}</span>
     </li>
     <li @if($_pageName == 'Video') class="active" @endif>
       <a href="{{URL::to('charity')}}/{{$_id}}/video">วีดีโอ</a>
+      <span class="badge badge-blue rounded-x">{{$_totalVideo}}</span>
     </li>
     <li><a href="{{URL::to('donate')}}?for=charity&id={{$_id}}">บริจาค</a></li>
   </ul>
